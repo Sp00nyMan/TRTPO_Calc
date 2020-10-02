26ec8d64b3e5e3d7c85bbc552aa95596c58b7779
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-10-02 11:46:11 +0300

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TRTPO_CALC
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.MainContainer = new System.Windows.Forms.SplitContainer();
+			this.Container1 = new System.Windows.Forms.SplitContainer();
+			this.Container1_1 = new System.Windows.Forms.SplitContainer();
+			this.Container2 = new System.Windows.Forms.SplitContainer();
+			this.Container2_1 = new System.Windows.Forms.SplitContainer();
+			((System.ComponentModel.ISupportInitialize)(this.MainContainer)).BeginInit();
+			this.MainContainer.Panel1.SuspendLayout();
+			this.MainContainer.Panel2.SuspendLayout();
+			this.MainContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.Container1)).BeginInit();
+			this.Container1.Panel1.SuspendLayout();
+			this.Container1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.Container1_1)).BeginInit();
+			this.Container1_1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.Container2)).BeginInit();
+			this.Container2.Panel1.SuspendLayout();
+			this.Container2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.Container2_1)).BeginInit();
+			this.Container2_1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// MainContainer
+			// 
+			this.MainContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.MainContainer.IsSplitterFixed = true;
+			this.MainContainer.Location = new System.Drawing.Point(3, 3);
+			this.MainContainer.Margin = new System.Windows.Forms.Padding(0);
+			this.MainContainer.Name = "MainContainer";
+			this.MainContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// MainContainer.Panel1
+			// 
+			this.MainContainer.Panel1.Controls.Add(this.Container1);
+			// 
+			// MainContainer.Panel2
+			// 
+			this.MainContainer.Panel2.Controls.Add(this.Container2);
+			this.MainContainer.Size = new System.Drawing.Size(374, 474);
+			this.MainContainer.SplitterDistance = 235;
+			this.MainContainer.TabIndex = 0;
+			this.MainContainer.TabStop = false;
+			// 
+			// Container1
+			// 
+			this.Container1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.Container1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Container1.IsSplitterFixed = true;
+			this.Container1.Location = new System.Drawing.Point(0, 0);
+			this.Container1.Margin = new System.Windows.Forms.Padding(0);
+			this.Container1.Name = "Container1";
+			this.Container1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// Container1.Panel1
+			// 
+			this.Container1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.Container1.Panel1.Controls.Add(this.Container1_1);
+			// 
+			// Container1.Panel2
+			// 
+			this.Container1.Panel2.BackColor = System.Drawing.Color.Fuchsia;
+			this.Container1.Size = new System.Drawing.Size(374, 235);
+			this.Container1.SplitterDistance = 156;
+			this.Container1.TabIndex = 0;
+			this.Container1.TabStop = false;
+			// 
+			// Container1_1
+			// 
+			this.Container1_1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.Container1_1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Container1_1.IsSplitterFixed = true;
+			this.Container1_1.Location = new System.Drawing.Point(0, 0);
+			this.Container1_1.Name = "Container1_1";
+			this.Container1_1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// Container1_1.Panel1
+			// 
+			this.Container1_1.Panel1.BackColor = System.Drawing.SystemColors.Desktop;
+			// 
+			// Container1_1.Panel2
+			// 
+			this.Container1_1.Panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.Container1_1.Size = new System.Drawing.Size(374, 156);
+			this.Container1_1.SplitterDistance = 76;
+			this.Container1_1.TabIndex = 0;
+			this.Container1_1.TabStop = false;
+			// 
+			// Container2
+			// 
+			this.Container2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Container2.IsSplitterFixed = true;
+			this.Container2.Location = new System.Drawing.Point(0, 0);
+			this.Container2.Name = "Container2";
+			this.Container2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// Container2.Panel1
+			// 
+			this.Container2.Panel1.Controls.Add(this.Container2_1);
+			// 
+			// Container2.Panel2
+			// 
+			this.Container2.Panel2.BackColor = System.Drawing.Color.Fuchsia;
+			this.Container2.Size = new System.Drawing.Size(374, 235);
+			this.Container2.SplitterDistance = 156;
+			this.Container2.TabIndex = 0;
+			this.Container2.TabStop = false;
+			// 
+			// Container2_1
+			// 
+			this.Container2_1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Container2_1.IsSplitterFixed = true;
+			this.Container2_1.Location = new System.Drawing.Point(0, 0);
+			this.Container2_1.Name = "Container2_1";
+			this.Container2_1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// Container2_1.Panel1
+			// 
+			this.Container2_1.Panel1.BackColor = System.Drawing.SystemColors.Desktop;
+			// 
+			// Container2_1.Panel2
+			// 
+			this.Container2_1.Panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.Container2_1.Size = new System.Drawing.Size(374, 156);
+			this.Container2_1.SplitterDistance = 76;
+			this.Container2_1.TabIndex = 0;
+			this.Container2_1.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(380, 480);
+			this.Controls.Add(this.MainContainer);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Padding = new System.Windows.Forms.Padding(3);
+			this.Text = "CalCon";
+			this.Shown += new System.EventHandler(this.MainForm_Shown);
+			this.MainContainer.Panel1.ResumeLayout(false);
+			this.MainContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.MainContainer)).EndInit();
+			this.MainContainer.ResumeLayout(false);
+			this.Container1.Panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.Container1)).EndInit();
+			this.Container1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.Container1_1)).EndInit();
+			this.Container1_1.ResumeLayout(false);
+			this.Container2.Panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.Container2)).EndInit();
+			this.Container2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.Container2_1)).EndInit();
+			this.Container2_1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer MainContainer;
+		private System.Windows.Forms.SplitContainer Container2;
+		private System.Windows.Forms.SplitContainer Container2_1;
+		private System.Windows.Forms.SplitContainer Container1;
+		private System.Windows.Forms.SplitContainer Container1_1;
+	}
+}
+
